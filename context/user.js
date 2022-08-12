c97e4ae7e91fe1f1122401a66834e5e7fecd8a8f
pavanleton <pavankumarkpkp@gmail.com>
2022-08-12 16:50:51 +0530

    update: add user context

---
+++ context/user.js
@@ -0,0 +1,31 @@
+import React, { ReactChild, useState } from "react";
+
+export const UserContext = React.createContext({
+    profile: {
+        fullname: "",
+        username: "",
+        email: "",
+        avatar: "",
+        bio: "",
+    },
+});
+
+export const UserProvider = ({ children }) => {
+    const [profile, setProfile] = useState({
+        fullname: "",
+        username: "",
+        email: "",
+        avatar: "",
+        bio: "",
+    });
+
+    const updateProfile = (newProfile) => {
+        setProfile(newProfile);
+    }
+
+    return (
+        <UserContext.Provider value={{ profile, updateProfile }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { instagramTheme } from "../theme";
 
+// CONTEXT
+import {UserProvider} from "../context/user";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={instagramTheme}>
-      <Component {...pageProps} />
+        <UserProvider>
+            <Component {...pageProps} />
+        </UserProvider>
     </ChakraProvider>
   );
 }
